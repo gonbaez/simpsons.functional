240d9f5d706b9bc5a7201bba243d1c13eb0790b5
gonbaez <gon.baez@gmail.com>
2024-02-25 22:27:01 +0000

    added routes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import styles from "../styles/Footer.module.css";
 
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <>
       <div className={styles.footerContainer}>
-        <small>&copy; Gonzalo Baez 2024</small>
+        <small>
+          &copy; <Link to="about">Gonzalo Baez</Link> 2024
+        </small>
       </div>
     </>
   );
